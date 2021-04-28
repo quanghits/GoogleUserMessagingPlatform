fe5368f93c1fdd17c331db9a015b1e7bbfe2c1e1
TA MINH QUANG <quang@TAs-Mac-mini.local>
2021-04-28 22:27:02 +0700

    Import weak linekd frameworks

---
+++ Sources/weak_import.swift
@@ -0,0 +1,12 @@
+//
+//  weak_import.swift
+//  
+//
+//  Created by Kazuma Koze on 2021/03/10.
+//
+
+// ref. https://github.com/CocoaPods/Specs/blob/00c12183a9d43a1665d74bff312058307c72a59d/Specs/7/e/f/GoogleUserMessagingPlatform/1.4.0/GoogleUserMessagingPlatform.podspec.json#L25-L27
+
+#if canImport(AdSupport)
+import AdSupport
+#endif
